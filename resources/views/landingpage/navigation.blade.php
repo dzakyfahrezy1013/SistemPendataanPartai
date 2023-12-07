c1b71d103cf593bafa483b680bf5156ea754a16a
dzakyfahrezy1013 <20sa1013@mhs.amikompurwokerto.ac.id>
2023-12-07 20:57:38 +0700

    update navigation

--- resources/views/landingpage/navigation.blade.php
+++ resources/views/landingpage/navigation.blade.php
@@ -5,9 +5,9 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
                 <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="resume.html">Resume</a></li>
-                <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
-                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="resume.html">Visi & Misi</a></li>
+                <li class="nav-item"><a class="nav-link" href="projects.html">Program Kerja</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Kegiatan</a></li>
             </ul>
         </div>
     </div>
